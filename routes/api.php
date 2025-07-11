205013d57a68e882f96763f689c41e0fab5d8627
Lioncode_User_02 <eugmous@lioncode.gr>
2025-07-11 13:09:42 +0300

    Finished

--- routes/api.php
+++ routes/api.php
@@ -9,12 +9,12 @@ Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
-Route::apiResource('songs',SongController::class)->scoped()->middleware('auth:sanctum');
-
 Route::post('/login', [AuthController::class,'login']);
 
 Route::post('/logout', [AuthController::class,'logout'])->middleware('auth:sanctum');
 
 Route::post('/register', [AuthController::class, 'register']);
+
+Route::apiResource('songs',SongController::class)->scoped()->middleware('auth:sanctum');
 
 Route::get('/dashboard', [SongController::class,'index'])->middleware('auth:sanctum');
